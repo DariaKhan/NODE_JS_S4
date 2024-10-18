693ff18f055f55f59cc99a55a825efc5bc18c070
Daria_Khan <tesurus2005@yahoo.com>
2024-10-18 22:09:25 +0200

    correction id

--- validation/scheme.js
+++ validation/scheme.js
@@ -1,7 +1,6 @@
 const Joi = require('joi');
 
 const userScheme = Joi.object({
-    id: Joi.number().integer().required(),
     firstName: Joi.string().min(1).required(),
     secondName: Joi.string().min(1).required(),
     age: Joi.number().integer().min(0).max(150).required(),
